f8b8c00297db76abf657a9dbe68ea27624e54cee
Fadi Alladkani <fadi.alladkani@outlook.com>
2018-01-14 18:33:21 +0200

    added basic server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var fs = require("fs");
 var http = require("http");
 var path = require("path");
+var mime = require("mime");
 var scores;
 
 fs.exists("list.json", function(exists){
@@ -13,5 +14,26 @@ function readSheet(){
       console.log("PANIC");
     else
       scores = JSON.parse(data);
+  });
+}
+var server = http.createServer(function(request, response){
+  if(request.url == "/")
+    serveFile("./client/page.html", response);
+  else{
+    console.log("Serving:", "." + request.url);
+    serveFile("." + request.url, response);
+  }
+});
+server.listen(8989, function(){
+  console.log("Server listening on port 8989");
+});
+function serveFile(filePath, response){
+  fs.readFile(filePath, function(err, data){
+    if(!err){
+      console.log("File found");
+      response.writeHead(200, {"Content-Type": mime.getType(filePath)});
+      response.write(data);
+    }
+    response.end();
   });
 }
