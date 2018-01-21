db679df57dd00562584b5909d69659420fecad2c
Fadi Alladkani <fadi.alladkani@outlook.com>
2018-01-21 22:45:40 +0200

    added rank

--- client/javascripts/scoreList.js
+++ client/javascripts/scoreList.js
@@ -2,13 +2,14 @@ var list;
 $(document).ready(function(){
   $.getJSON("/list.json", function(data){
     list = data.list;
-    sort();
+    //sort();
     populate();
   });
 });
 function populate(){
   for(var i = 0; i < list.length; i++){
     var row = $("<tr></tr>");
+    row.append("<td>" + list[i].rank + "</td>");
     row.append("<td>" + list[i].name + "</td>");
     row.append("<td>" + list[i].score + "</td>");
     $("#cTable").append(row);
