24156e7627880bb9f7ad193285ffc19eaad135b2
Fadi Alladkani <fadi.alladkani@outlook.com>
2018-01-31 14:46:37 +0200

    removed useless code

--- client/javascripts/scoreList.js
+++ client/javascripts/scoreList.js
@@ -1,22 +1,2 @@
-var list;
 $(document).ready(function(){
-  $.getJSON("/list.json", function(data){
-    list = data.list;
-    sort();
-    populate();
-  });
 });
-function populate(){
-  for(var i = 0; i < list.length; i++){
-    var row = $("<tr></tr>");
-    row.append("<td>" + list[i].rank + "</td>");
-    row.append("<td>" + list[i].name + "</td>");
-    row.append("<td>" + list[i].score + "</td>");
-    $("#cTable").append(row);
-  }
-}
-function sort(){
-  list.sort(function(a, b){
-    return a.rank - b.rank;
-  });
-}
