a89cc1960ba95e73ad9a7a6e1601a707c7af9091
Fadi Alladkani <fadi.alladkani@outlook.com>
2018-02-02 21:59:28 +0200

    integrated gulp build

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,29 @@
 var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var cleanCSS = require('gulp-clean-css');
+var del = require('del');
+
+gulp.task('clean:dist', function(){
+  return del.sync('client/build');
+});
+gulp.task('minJS', function(){
+  return gulp.src('client/javascripts/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('client/build/javascripts'));
+});
+gulp.task('minCSS', function(){
+  return gulp.src('client/stylesheets/*.css')
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('client/build/stylesheets'));
+});
+gulp.task('icons', function(){
+  return gulp.src('client/icons/*')
+    .pipe(gulp.dest('client/build/icons'));
+});
+gulp.task('html', function(){
+  return gulp.src('client/*.html')
+    .pipe(gulp.dest('client/build'));
+});
+gulp.task('build', ['minJS', 'minCSS', 'icons', 'html'], function(){
+
+});
