2c7f076488d67e0c43259a5b4fb6705a0e0dee3f
Fadi Alladkani <fadi.alladkani@outlook.com>
2018-02-04 13:32:55 +0200

    moved server startup code to method

--- server.js
+++ server.js
@@ -24,53 +24,49 @@ var intervalId = setInterval(function(){
   }
 }, 1000);
 
-//SERVER METHODS
-
-app.use(log('dev'));
-app.use(bodyParser.urlencoded({extended: false}));
-app.use('/list.json', listR(db));
-app.get('/', function(req, res, next){
-  res.sendFile(path.join(__dirname, 'client/page.html'));
-});
-if(process.env.NODE_ENV === 'development'){
-  console.log('dev env detected');
-  app.use(express.static(path.join(__dirname, 'client')));
-}
-else {
-  console.log("prod env detected");
-  app.use(express.static(path.join(__dirname, 'client/build')));
-}
-app.get('/scores', function(req, res, next){
-  var str = ''+req.query.rank;
-  var num = str.slice(0, str.length-1) + '0';
-  var range = req.query.range;
-  db.query(
-    "SELECT name, score, rank FROM scores WHERE rank BETWEEN "
-     + num + " AND " + (Number(num)+ Number(range))
-     + " ORDER BY rank;"
-  , function(err, data){
-    db.query(
-      "show table status;", function(err, stats){
-        var p1 = Number(stats[1].Rows)/Number(range); //Number of rows per page
-        //if p1 has a decimel, then we need one more page for the remainder
-        var pages = (p1 + "").indexOf('.') > 0 ? Math.floor(p1+1) : p1;
-        var curPage = Math.floor(Number(num)/Number(range));
-        res.render('scoreView', {scores: data, pages: pages, curPage: curPage});
-      }
-    );
-  });
-});
-
-app.set('port', 8080);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-//HELPER FUNCTIONS
-
 function startServer(){
+  app.use(log('dev'));
+  app.use(bodyParser.urlencoded({extended: false}));
+  app.use('/list.json', listR(db));
+  app.get('/', function(req, res, next){
+    res.sendFile(path.join(__dirname, 'client/page.html'));
+  });
+  if(process.env.NODE_ENV === 'development'){
+    console.log('dev env detected');
+    app.use(express.static(path.join(__dirname, 'client')));
+  }
+  else {
+    console.log("prod env detected");
+    app.use(express.static(path.join(__dirname, 'client/build')));
+  }
+  app.get('/scores', function(req, res, next){
+    var str = ''+req.query.rank;
+    var num = str.slice(0, str.length-1) + '0';
+    var range = req.query.range;
+    db.query(
+      "SELECT name, score, rank FROM scores WHERE rank BETWEEN "
+      + num + " AND " + (Number(num)+ Number(range))
+      + " ORDER BY rank;"
+      , function(err, data){
+        db.query(
+          "show table status;", function(err, stats){
+            var p1 = Number(stats[1].Rows)/Number(range); //Number of rows per page
+            //if p1 has a decimel, then we need one more page for the remainder
+            var pages = (p1 + "").indexOf('.') > 0 ? Math.floor(p1+1) : p1;
+            var curPage = Math.floor(Number(num)/Number(range));
+            res.render('scoreView', {scores: data, pages: pages, curPage: curPage});
+          }
+        );
+      });
+    });
+  app.set('port', 8080);
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'ejs');
   var server = http.createServer(app);
   server.listen(8080);
 }
+
+//HELPER FUNCTIONS
 
 function createTable(){
   dbPingDone = false;
